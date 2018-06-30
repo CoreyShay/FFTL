4cc1f33da77af0f0294297c11070915f3abca089
CoreyShay <cshay892@gmail.com>
2018-06-29 17:41:44 -0700

    Threading code for reference.
    This codebase is going to have to bifurcate at some point.

--- FFTL/defs.h
+++ FFTL/defs.h
@@ -2,7 +2,7 @@
 
 Original author:
 Corey Shay
-cshay892@gmail.com
+corey@signalflowtechnologies.com
 
 This is free and unencumbered software released into the public domain.
 
@@ -32,92 +32,215 @@ OTHER DEALINGS IN THE SOFTWARE.
 #ifndef FFTL_DEFS_H
 #define FFTL_DEFS_H
 
-
-#if !defined(__cplusplus)
-#error C compilers not supported.
+#include <type_traits>
+#include <stddef.h>
+#if defined(__GNUC__) || defined(__clang__)
+#	include <unistd.h>
 #endif
 
 
-#ifndef NULL
-#define NULL 0
+#if !defined(__cplusplus)
+#	error C compilers not supported.
+#endif
+
+#if (__cplusplus >= 201703L) || (defined(_MSC_VER) && _MSC_VER >= 1911)
+#	define FFTL_CPP_VERSION 17
+#else
+#	define FFTL_CPP_VERSION 14
+#endif
+
+
+#if (FFTL_CPP_VERSION >= 17)
+#	define FFTL_IF_CONSTEXPR(__expr__) if constexpr (__expr__)
+#else
+#	define FFTL_IF_CONSTEXPR(__expr__) if (__expr__)
+#endif
+
+#if defined(_M_X64) || defined(__x86_64__) || defined(__aarch64__)
+#	define FFTL_64BIT 1
 #endif
 
 //	For vector intrinsics on non-x86 platforms (PowerPC/ARM), this can easily be modified to use them.
 // Define your symbol for it here, and create your own .inl file that defines all the necessary math
-// functions. Don't forget to include that .inl file at the bottom of Math.h.
-#if (defined(_M_IX86_FP) && _M_IX86_FP > 0) || defined(_M_X64) || defined(__AVX__) || defined(FFTL_USE_SSE)
-	#define FFTL_SSE 1
-#endif
-#if (defined(_M_IX86_FP) && _M_IX86_FP > 1) || defined(_M_X64) || defined(__AVX__) || defined(FFTL_USE_SSE2)
-	#define FFTL_SSE2 1
-#endif
-#if defined(__AVX__) || defined(FFTL_USE_AVX)
-	#define FFTL_SSE3 1
-	#define FFTL_SSE4 1
-	#define FFTL_AVX 1
+// functions. Don't forget to include that .inl file at the bottom of MathCommon.h.
+#if defined(__AVX2__) || defined(FFTL_USE_AVX2)
+#	define FFTL_SSE 1
+#	define FFTL_SSE2 1
+#	define FFTL_SSE3 1
+#	define FFTL_SSE4 1
+#	define FFTL_AVX 1
+#	define FFTL_AVX2 1
+#	define FFTL_FMA3 1
+#elif defined(__AVX__) || defined(FFTL_USE_AVX)
+#	define FFTL_SSE 1
+#	define FFTL_SSE2 1
+#	define FFTL_SSE3 1
+#	define FFTL_SSE4 1
+#	define FFTL_AVX 1
+#elif defined(__SSE4_2__) || defined(FFTL_USE_SSE4)
+#	define FFTL_SSE 1
+#	define FFTL_SSE2 1
+#	define FFTL_SSE3 1
+#	define FFTL_SSE4 1
+#elif defined(__SSE3__) || defined(FFTL_USE_SSE3)
+#	define FFTL_SSE 1
+#	define FFTL_SSE2 1
+#	define FFTL_SSE3 1
+#elif defined(__SSE2__) || (defined(_M_IX86_FP) && _M_IX86_FP > 1) || defined(_M_X64) || defined(FFTL_USE_SSE2)
+#	define FFTL_SSE 1
+#	define FFTL_SSE2 1
+#elif defined(__SSE__) || (defined(_M_IX86_FP) && _M_IX86_FP > 0) || defined(_M_X64) || defined(FFTL_USE_SSE)
+#	define FFTL_SSE 1
 #endif
 
-#if defined(__ARM_NEON__)
-	#define FFTL_ARM_NEON 1
+#if !defined(FFTL_FMA3) && (defined(FFTL_USE_FMA3) || defined(__FMA__))
+#	define FFTL_FMA3 1
+#endif
+#if !defined(FFTL_FMA4) && defined(FFTL_USE_FMA4)
+#	define FFTL_FMA4 1
 #endif
 
-#if FFTL_SSE || FFTL_ARM_NEON
-	#define FFTL_SIMD4 1
+#if defined(FFTL_SSE)
+#	define FFTL_SSE_ONLY(__stuff__) __stuff__
 #else
-	#define FFTL_SIMD4 0
+#	define FFTL_SSE_ONLY(__stuff__)
 #endif
-#if FFTL_AVX
-	#define FFTL_SIMD8 1
+#if defined(FFTL_SSE2)
+#	define FFTL_SSE2_ONLY(__stuff__) __stuff__
 #else
-	#define FFTL_SIMD8 0
+#	define FFTL_SSE2_ONLY(__stuff__)
+#endif
+#if defined(FFTL_SSE3)
+#	define FFTL_SSE3_ONLY(__stuff__) __stuff__
+#else
+#	define FFTL_SSE3_ONLY(__stuff__)
+#endif
+#if defined(FFTL_SSE4)
+#	define FFTL_SSE4_ONLY(__stuff__) __stuff__
+#else
+#	define FFTL_SSE4_ONLY(__stuff__)
+#endif
+#if defined(FFTL_AVX)
+#	define FFTL_AVX_ONLY(__stuff__) __stuff__
+#else
+#	define FFTL_AVX_ONLY(__stuff__)
+#endif
+#if defined(FFTL_AVX2)
+#	define FFTL_AVX2_ONLY(__stuff__) __stuff__
+#else
+#	define FFTL_AVX2_ONLY(__stuff__)
 #endif
 
-#if defined(_MSC_VER)
-	#define FFTL_ALIGN_BEGIN(x) __declspec( align(x) )
-	#define FFTL_ALIGN_END(x)
-#else
-	#define FFTL_ALIGN_BEGIN(x)
-	#define FFTL_ALIGN_END(x) __attribute__((aligned(x)))
+#if defined(__ORBIS__) || defined(_DURANGO)
+#	define FFTL_CPU_INFO_KNOWN_TO_COMPILER 1
 #endif
 
-#if (__cplusplus >= 201103L) || (defined(_MSC_VER) && _MSC_VER >= 1600)
+#if defined(__ARM_NEON) || defined(__ARM_NEON__)
+#	define FFTL_ARM_NEON 1
+#endif
+
+#if defined(FFTL_SSE) && defined(_MSC_VER) && _MSC_VER >= 1700
+#	define FFTL_VECTORCALL __vectorcall
+#elif defined(FFTL_SSE) && defined(__GNUC__)
+#	define FFTL_VECTORCALL __attribute__((sseregparm))
+#else
+#	define FFTL_VECTORCALL
+#endif
+
+#if defined(FFTL_SSE) || defined(FFTL_ARM_NEON)
+#	define FFTL_SIMD4 1
+#endif
+#if defined(FFTL_AVX)
+#	define FFTL_SIMD8 1
+#endif
+
+#if defined(FFTL_SIMD8)
+#	define FFTL_SIMD_WIDTH 8
+#elif defined(FFTL_SIMD4)
+#	define FFTL_SIMD_WIDTH 4
+#else
+#	define FFTL_SIMD_WIDTH 1
+#endif
+
 #define FFTL_StaticAssert(e) static_assert(e, "Compile-time assert triggered!")
-#else
-#define FFTL_MacroJoin( _X, sh_Y ) FFTL_MacroDoJoin(_X, sh_Y)
-#define FFTL_MacroDoJoin( _X, sh_Y ) FFTL_MacroDoJoin2(_X, sh_Y)
-#define FFTL_MacroDoJoin2( _X, sh_Y ) _X##sh_Y
-#define FFTL_StaticAssert(e) typedef char FFTL_MacroJoin(FFTL_MacroJoin(__COMPILETIME_ASSERT, __LINE__), __)[(e)?1:-1]
-#endif
 
 
-#if defined(_DEBUG) || defined(FFTL_ENABLE_ASSERT)
-	#if defined(_MSC_VER)
-		#define FFTL_ASSERT(___expr) (void)( (!!(___expr)) || ( __debugbreak(), 1) )
-	#else
-		#define FFTL_ASSERT(___expr) (void)( (!!(___expr)) || ( *((int*)0) = 0xdeadbeef ) )
-	#endif
+#if defined(FFTL_ENABLE_ASSERT)
+#	define FFTL_BUILD_QA 1
+#	if defined(_MSC_VER) || defined(__ORBIS__)
+#		define FFTL_ASSERT(___expr) (void)( (!!(___expr)) || ( __debugbreak(), 1) )
+#	elif defined(_POSIX_VERSION)
+#		define FFTL_ASSERT(___expr) (void)( (!!(___expr)) || ( raise(SIGTRAP), 1) )
+#	else
+#		define FFTL_ASSERT(___expr) (void)( (!!(___expr)) || ( *((int*)0) = 0xdeadbeef ) )
+#	endif
+
+#	define FFTL_VERIFY(___expr) FFTL_ASSERT(___expr)
+#	define FFTL_VERIFY_EQ(___cmp, ___expr) FFTL_ASSERT(___cmp == ___expr)
+#	define FFTL_VERIFY_NEQ(___cmp, ___expr) FFTL_ASSERT(___cmp != ___expr)
+
 #else
-	#define FFTL_ASSERT(___expr) ((void)0)
+#	define FFTL_ASSERT(___expr) ((void)0)
+#	define FFTL_VERIFY(___expr) ___expr
+#	define FFTL_VERIFY_EQ(___cmp, ___expr) ___expr
+#	define FFTL_VERIFY_NEQ(___cmp, ___expr) ___expr
+
+#	if defined(_MSC_VER)
+#		undef FFTL_VERIFY
+#		define FFTL_VERIFY(___expr)			\
+		__pragma(warning(push))				\
+		__pragma(warning(disable : 4552))	\
+		__pragma(warning(disable : 4553))	\
+		___expr								\
+		__pragma(warning(pop))
+#	endif
 #endif
+
+#define FFTL_ASSERT_MSG(___expr, msg, ...) FFTL_ASSERT(___expr)
+#define FFTL_VERIFY_MSG(___expr, msg, ...) FFTL_VERIFY(___expr)
+#define FFTL_VERIFY_EQ_MSG(___cmp, ___expr, msg, ...) FFTL_VERIFY_EQ(___cmp, ___expr)
+#define FFTL_VERIFY_NEQ_MSG(___cmp, ___expr, msg, ...) FFTL_VERIFY_NEQ(___cmp, ___expr)
 
 
 #define FFTL_INLINE inline
 
 #if defined(_MSC_VER)
-	#define FFTL_FORCEINLINE __forceinline
-	#define FFTL_NOINLINE __declspec(noinline)
+#	define FFTL_FORCEINLINE __forceinline
+#	define FFTL_NOINLINE __declspec(noinline)
+#	if defined(FFTL_DLL)
+#		define FFTL_DLLEXPORT __declspec(dllexport)
+#	else
+#		define FFTL_DLLEXPORT __declspec(dllimport)
+#	endif
 #elif defined(__ANDROID__)
-	#define FFTL_FORCEINLINE inline __attribute__((always_inline))
-	#define FFTL_NOINLINE __attribute((noinline))
+#	define FFTL_FORCEINLINE inline __attribute__((always_inline))
+#	define FFTL_NOINLINE __attribute((noinline))
+#	ifdef FFTL_DLL
+#		define FFTL_DLLEXPORT __attribute__((visibility("default")))
+#	else
+#		define FFTL_DLLEXPORT
+#	endif
 #else
-	#define FFTL_FORCEINLINE inline __attribute__((always_inline))
-	#define FFTL_NOINLINE __noinline
+#	define FFTL_FORCEINLINE inline __attribute__((always_inline))
+#	define FFTL_NOINLINE __noinline
+#	ifdef FFTL_DLL
+#		define FFTL_DLLEXPORT __declspec(dllexport)
+#	else
+#		define FFTL_DLLEXPORT __declspec(dllimport)
+#	endif
 #endif
 
 #if defined(FFTL_FORCE_COND_INLINE)
-	#define FFTL_COND_INLINE FFTL_FORCEINLINE
+#	define FFTL_COND_INLINE FFTL_FORCEINLINE
 #else
-	#define FFTL_COND_INLINE FFTL_NOINLINE
+#	define FFTL_COND_INLINE FFTL_NOINLINE
+#endif
+
+#define FFTL_UNLIKELY(___expr) ___expr
+
+
+#if defined(_MSC_VER)
+#	pragma warning(disable : 4251) // 'identifier' : class 'type' needs to have dll-interface to be used by clients of class 'type2'
 #endif
 
 
@@ -125,52 +248,40 @@ namespace FFTL
 {
 
 
-typedef unsigned char		uint8;
-typedef char				int8;
-typedef uint8				byte;
-typedef unsigned short		uint16;
-typedef short				int16;
-typedef unsigned long		uint32;
-typedef unsigned long		uint;
-typedef long				int32;
-typedef unsigned long long	uint64;
-typedef long long			int64;
+typedef unsigned char		u8;
+typedef char				s8;
+typedef unsigned short		u16;
+typedef short				s16;
+typedef unsigned int		u32;
+typedef unsigned int		uint;
+typedef int					s32;
+typedef unsigned long long	u64;
+typedef long long			s64;
 typedef float				f32;
 typedef double				f64;
 
+//	Aliases and shorthands
+typedef u8					byte;
 
 
-template <typename T, uint T_N>
-class FixedArray
-{
-public:
-	T data[T_N];
-	FFTL_FORCEINLINE T& operator[](uint32 n) { FFTL_ASSERT(n<T_N); return data[n]; }
-	FFTL_FORCEINLINE const T& operator[](uint n) const { FFTL_ASSERT(n<T_N); return data[n]; }
-	FFTL_FORCEINLINE T* operator+(uint n) { FFTL_ASSERT(n<T_N); return data+n; }
-	FFTL_FORCEINLINE const T* operator+(uint n) const { FFTL_ASSERT(n<T_N); return data+n; }
-	FFTL_FORCEINLINE static uint size() { return T_N; }
-};
 
-template <typename T, uint32 T_N>
-class FFTL_ALIGN_BEGIN(16) FixedArray_Aligned16 : public FixedArray<T,T_N>
-{
-} FFTL_ALIGN_END(16);
-
-template <typename T, uint32 T_N>
-class FFTL_ALIGN_BEGIN(32) FixedArray_Aligned32 : public FixedArray<T,T_N>
-{
-} FFTL_ALIGN_END(32);
-
-template<uint T_ALIGNMENT, typename T>
-inline T AlignForward(const T& offset)
-{
-	FFTL_StaticAssert( (T_ALIGNMENT & (T_ALIGNMENT-1)) == 0);
-	return (offset + (T_ALIGNMENT-1)) & ~(T_ALIGNMENT-1);
-}
 
 
 } // namespace FFTL
+
+
+
+
+
+#define FFTL_INVALID_SAMPLE_CLOCK 0xffffffffffffffff
+#define FFTL_MAX_FILTER_CUTOFF_HZ (24000)
+#define FFTL_MIN_FILTER_CUTOFF_HZ (0)
+
+#if /*defined(__ORBIS__) ||*/ defined(__ANDROID__)
+#	define FFTL_THREAD_USE_POSIX 1
+#endif
+
+
 
 
 #endif
